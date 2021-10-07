c4cf09ac46fc133db180c8b48b6b4047ced25783
Izra Faturrahman <Frizz925@hotmail.com>
2021-10-07 07:49:42 +0700

    feat: Return 503 status on forward error

--- internal/filter/forward_filter.go
+++ internal/filter/forward_filter.go
@@ -4,6 +4,7 @@ import (
 	"net"
 	"net/http"
 
+	"github.com/frizz925/higuchi/internal/errors"
 	"go.uber.org/zap"
 )
 
@@ -25,9 +26,23 @@ func (ff *ForwardFilter) Do(c *Context, req *http.Request) error {
 	addr := net.JoinHostPort(host, port)
 	c.Logger = c.Logger.With(zap.String("dst", addr))
 	for _, f := range ff.filters {
-		if err := f.Do(c, addr); err != nil {
-			return err
+		if err = f.Do(c, addr); err != nil {
+			break
 		}
+	}
+	if err != nil {
+		he := &errors.HTTPError{
+			Err:         err.Error(),
+			Source:      c.RemoteAddr(),
+			Listener:    c.LocalAddr(),
+			Destination: addr,
+			Request:     req,
+			StatusCode:  http.StatusServiceUnavailable,
+		}
+		if req.URL != nil && req.URL.User != nil {
+			he.User = req.URL.User
+		}
+		return he
 	}
 	return nil
 }
